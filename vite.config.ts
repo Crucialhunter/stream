51a90c77c58807218804d715d9c34b03f410ce4c
Crucialhunter <misjuegossonmios@gmail.com>
2025-12-01 22:33:40 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are loaded correctly on GitHub Pages
+  // IMPORTANT: repo is served under /stream/ on GitHub Pages
+  base: '/stream/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false
-  }
+    sourcemap: false,
+  },
 });
